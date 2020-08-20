eff530c765c66f37db5812d46111440d6ec853d5
ayushi <ayushiparmar2011@gmail.com>
2020-08-20 12:19:21 +0530

    edited exports next

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  next();
 });
 
-module.exports = router;
+//module.exports = router;
+
+module.exports = router1;
